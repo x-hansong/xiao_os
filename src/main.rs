1bd808a4c56899c14c186b27a708514b4030e2cf
hansong.xhs <hansong.xhs@gmail.com>
2020-03-04 09:10:05 +0800

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(xiao_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-mod vga_buffer;
+use xiao_os::println;
 
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}",info);
     loop{}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    xiao_os::test_panic_handler(info)
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+        test_main();
+
     loop{}
 }
+
---
+++ tests/basic_boot.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![reexport_test_harness_main = "test_main"]
+#![test_runner(xiao_os::test_runner)]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    xiao_os::test_panic_handler(info)
+}
+
+use xiao_os::{println, serial_print, serial_println};
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
+}
+
